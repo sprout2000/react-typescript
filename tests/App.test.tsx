5a4d407d4278e270c40a4d720d5265a7b459a499
sprout2000 <wassabie64@gmail.com>
2021-03-10 10:29:19 +0900

    chore: update App.test.tsx

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import { App } from '../src/App';
 
@@ -9,7 +8,10 @@ describe('App component', () => {
   test('render App component', () => {
     render(<App />);
 
-    expect(screen.getByText(/Learn React/)).toBeInTheDocument();
-    userEvent.click(screen.getByRole(/link/));
+    expect(screen.getByRole(/link/)).toHaveTextContent(/Learn React/i);
+    expect(screen.getByRole(/link/)).toHaveAttribute(
+      'href',
+      'https://reactjs.org'
+    );
   });
 });
