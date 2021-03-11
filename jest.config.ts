b3c0aa15a25d28bd8da10e6e1f34a13ac83b9b92
sprout2000 <wassabie64@gmail.com>
2021-03-11 16:49:25 +0900

    chore: update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,12 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ['<rootDir>/tests'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(ico|gif|jpe?g|png|svg|ttf|otf|eot|woff?2?)$':
-      '<rootDir>/mocks/fileMock.ts',
-    '\\.(css|scss)$': '<rootDir>/mocks/styleMock.ts',
+    '\\.(ico|gif|jpe?g|png|svg|ttf|otf|eot|woff?2?|mp3)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
   },
 };
 
